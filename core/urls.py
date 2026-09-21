64730eeb3c2997d2901246b8233ebe10a15f3bbd
VJ <johan.vergara0509x@gmail.com>
2026-09-21 11:08:59 -0500

    core urls,reposteria views y urls

--- core/urls.py
+++ core/urls.py
@@ -6,6 +6,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('accounts.urls')),
+    path('', include('reposteria.urls')),
 ]
 
 if settings.DEBUG:

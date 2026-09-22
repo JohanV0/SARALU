0f8aa975943ff60ac79a9ea97039878da557e2e8
Paula Tabares <tu-correo@ejemplo.com>
2026-09-22 09:35:12 -0500

    Home

--- reposteria/models.py
+++ reposteria/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class Producto(models.Model):
+    nombre = models.CharField(max_length=150)
+    descripcion = models.TextField(blank=True)
+    precio = models.DecimalField(max_digits=10, decimal_places=0)
+    imagen = models.ImageField(upload_to='productos/', blank=True, null=True)
+    destacado = models.BooleanField(default=False)
+    mas_vendido = models.BooleanField(default=False)
+    nuevo = models.BooleanField(default=False)
+    creado = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-creado']
+
+    def __str__(self):
+        return self.nombre

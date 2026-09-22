c14b39ca0b2ef5605ea76498b985410c0e38fb02
Paula Tabares <tu-correo@ejemplo.com>
2026-09-22 10:31:32 -0500

    Home

--- reposteria/admin.py
+++ reposteria/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from .models import Producto
 
+
 @admin.register(Producto)
 class ProductoAdmin(admin.ModelAdmin):
     list_display = ('nombre', 'precio', 'destacado', 'mas_vendido', 'nuevo', 'imagen')

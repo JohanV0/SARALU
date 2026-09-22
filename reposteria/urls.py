fa7002435553fb57b0e36b58af21a3ec41476216
Paula Tabares <tu-correo@ejemplo.com>
2026-09-22 09:16:14 -0500

    Home

--- reposteria/urls.py
+++ reposteria/urls.py
@@ -2,6 +2,9 @@ from django.http import HttpResponse
 from django.urls import path
 from . import views
 
+
 urlpatterns = [
-    path('', views.home, name = 'home')
+    path('', views.home, name='home'),
+    path('catalogo/', views.home, name='catalogo'),        # temporal
+    path('producto/<int:id>/', views.home, name='producto_detalle'),  # temporal
 ]
--- reposteria/views.py
+++ reposteria/views.py
@@ -2,6 +2,11 @@ from django.shortcuts import render , redirect, get_object_or_404
 from django.views.decorators.http import require_POST
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
+from .models import Producto
 
 def home(request):
-    return render(request,'home.html')
+    return render(request, 'home.html', {
+        'destacados':  Producto.objects.filter(destacado=True),
+        'mas_vendidos': Producto.objects.filter(mas_vendido=True),
+        'nuevos':      Producto.objects.filter(nuevo=True),
+    }) 
